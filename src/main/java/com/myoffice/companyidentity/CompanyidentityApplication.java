a4e6e2f99a1e655fabd0c26de3dc8e97617635c5
hemanthlucifer <hemanthkumardharanikota29@gmail.com>
2025-06-07 10:36:18 +0530

    initial commit

---
+++ src/main/java/com/myoffice/companyidentity/CompanyidentityApplication.java
@@ -0,0 +1,13 @@
+package com.myoffice.companyidentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyidentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyidentityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myoffice/companyidentity/CompanyidentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myoffice.companyidentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyidentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
