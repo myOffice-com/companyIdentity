164b61a6f7ddbdd3adb8c4d77d45e04a4490de8d
hemanthlucifer <hemanthkumardharanikota29@gmail.com>
2025-06-09 20:17:44 +0530

    created get company roles call

--- src/main/java/com/myoffice/companyidentity/repository/CompanyRolesRepository.java
+++ src/main/java/com/myoffice/companyidentity/repository/CompanyRolesRepository.java
@@ -2,6 +2,13 @@ package com.myoffice.companyidentity.repository;
 
 import com.myoffice.companyidentity.entity.CompanyRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CompanyRolesRepository extends JpaRepository<CompanyRoles,Long> {
+
+    List<CompanyRoles> findAllByCompanyId(String companyId);
+
 }
