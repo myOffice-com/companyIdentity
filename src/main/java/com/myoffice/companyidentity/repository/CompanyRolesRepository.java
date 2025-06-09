22f51d579d9f43059694d77e5f0c508b7c09ac7d
hemanthlucifer <hemanthkumardharanikota29@gmail.com>
2025-06-09 19:08:00 +0530

    Create CompanyRolesRepository.java

---
+++ src/main/java/com/myoffice/companyidentity/repository/CompanyRolesRepository.java
@@ -0,0 +1,7 @@
+package com.myoffice.companyidentity.repository;
+
+import com.myoffice.companyidentity.entity.CompanyRoles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRolesRepository extends JpaRepository<CompanyRoles,Long> {
+}
